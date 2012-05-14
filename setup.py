aca8a95abd93eb8ae6a416980ee961defd5ab840
John Cook <jcook793@gmail.com>
2012-05-14 15:06:57 -0400

    Updating version number

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ else:
 
 setup(
     name = 'Embedly',
-    version = 'batterii-0.1',
+    version = 'batterii-0.2',
     author = 'Embed.ly, Inc.',
     author_email = 'support@embed.ly',
     description = 'Python Library for Embedly',
